6616b4860ca5f817d7fdc40d7277d0685cd7b9fc
AntonyZI <anjablatru@gmail.com>
2021-07-21 10:32:57 -0500

    El ServerSocket del servidor contiene a un Stream que recibe información con el primer socket que se conecte

--- Server.java
+++ Server.java
@@ -2,21 +2,32 @@
 
 package javaserver;
 
+import java.io.DataInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
     private ServerSocket[] clientSvSockets;
+    private Socket[] clientSockets;
     
     public Server(){
         clientSvSockets = new ServerSocket[3];
+        clientSockets = new Socket[3];
         try {
-            clientSvSockets[1] = new ServerSocket(0);
+            clientSvSockets[0] = new ServerSocket(0);
             System.out.print("Server opened at ");
-            System.out.println(clientSvSockets[1].getInetAddress()+":"+clientSvSockets[1].getLocalPort());
+            System.out.println(clientSvSockets[0].getInetAddress().getHostName()+":"+clientSvSockets[0].getLocalPort());
             
-            clientSvSockets[1].close();
+            System.out.println("Waiting for the client'a socket . . .");
+            clientSockets[0] = clientSvSockets[0].accept();
+            System.out.println("Successfull client's socket connection");
+            
+            InputStream streamToClient = clientSockets[0].getInputStream();
+            DataInputStream dataStreamToClient = new DataInputStream(streamToClient);
+            
+            clientSvSockets[0].close();
         } catch (IOException ex) {
             System.out.println(ex.getMessage());
         }
