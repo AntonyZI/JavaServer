ff10f60ae0237c9f215128abdd9f1b6eb5587b25
AntonyZI <anjablatru@gmail.com>
2021-07-20 23:09:54 -0500

    El servidor puede abrir un ServerSocket, muestra su ip y el puerto

--- Server.java
+++ Server.java
@@ -2,6 +2,24 @@
 
 package javaserver;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
+    private ServerSocket[] clientSvSockets;
+    
+    public Server(){
+        clientSvSockets = new ServerSocket[3];
+        try {
+            clientSvSockets[1] = new ServerSocket(0);
+            System.out.print("Server opened at ");
+            System.out.println(clientSvSockets[1].getInetAddress()+":"+clientSvSockets[1].getLocalPort());
+            
+            clientSvSockets[1].close();
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
     
 }
